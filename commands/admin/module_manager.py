e227bc351a6d089b623165ec4bc419a950325edc
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2025-01-17 21:21:17 +0300

    Update module_manager.py

--- commands/admin/module_manager.py
+++ commands/admin/module_manager.py
@@ -124,7 +124,7 @@ async def catalog_modules(message: types.Message):
     global CATALOG
     
     try:
-        response = requests.get('https://raw.githubusercontent.com/Ijidishurka/bfg-modules/refs/heads/main/modules.json')
+        response = requests.get('https://raw.githubusercontent.com/Maksim534/bfg-modules/blob/main/modules.json')
         CATALOG = response.json()
     except:
         pass
