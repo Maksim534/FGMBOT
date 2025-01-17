6f43947b0fba8ceba20095c98739ca4e4fcd3cf4
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2025-01-17 21:22:49 +0300

    Update config_ex.py

--- config_ex.py
+++ config_ex.py
@@ -1,13 +1,13 @@
-API_TOKEN = ''
+API_TOKEN = '7692626473:AAEgJxf5PS_RG0_geBIqP3V8UU1uKlzmwl8'
 
-admin = []
+admin = [6888643375]
 start_money = 10000
 
-bot_name = 'BFG'
-chat = 't.me/copybfg'
-chanell = 't.me/copybfg'
-admin_username = '@copybfg'
-bot_username = 'bfgcopybot'
+bot_name = 'FGM'
+chat = 't.me/@geormor'
+chanell = 't.me/geormor'
+admin_username = '@geormor'
+bot_username = 'FGM'
 
 chat_log = 0
 cleaning = 60
