a3936b141f0b7b89a2b53b50f860075738c90798
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2025-01-28 23:27:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,11 @@ admin = [6888643375, 5244814453, 1688468160, 7418347376, 1852919832]
 start_money = 10000
 
 bot_name = 'FGM'
-chat = 't.me/@geormor'
-chanell = 't.me/geormor'
+chat = 't.me/FGMOFICIAL'
+chanell = 't.me/FGMINFO'
 admin_username = '@geormor'
 bot_username = 'FGM'
+
 
 chat_log = -1002247244049
 cleaning = 60
