62ade84321c2bab8e6c6193e6617287669a78737
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2025-01-30 22:06:19 +0300

    Update main.py

--- commands/Youtube/main.py
+++ commands/Youtube/main.py
@@ -2,8 +2,11 @@ from aiogram import Dispatcher, types
 from user import BFGuser, BFGconst
 import random
 from assest.antispam import antispam
+from commands.Youtube.dp as db
+from assest.transform import transform_int as tr
 
 
+@antispam
 async def youtube(message: types.Message, user: BFGuser):
   if int(user.property.phone) == 0:
       await message.answer('у вас нет телефона чтобы снимать видеоролики')
