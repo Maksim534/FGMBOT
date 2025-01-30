3bfe0bc67c6b0ace805eb9ec94979516f55eb3e4
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2025-01-30 22:28:42 +0300

    Update db.py

--- commands/Youtube/db.py
+++ commands/Youtube/db.py
@@ -1,1 +1,10 @@
+from decimal import Decimal
+from commands.db import conn, cursor
 
+
+async def buy_property(user_id: int, summ)
+    ecoins = cursor.execute('SELECT ecoins FROM users WHERE user_id = ?', (user_id,)).fetchone()[0]
+    summ = int(Decimal(ecoins) + Decimal(summ))
+
+    cursor.execute(f"UPDATE users SET ecoins = ? WHERE user_id = ?", (str(summ), user_id))
+    conn.commit()
