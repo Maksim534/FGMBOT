e9b2c3874911c381ea5329a6f0e9654855c2a1b1
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2025-01-30 21:44:02 +0300

    Update main.py

--- commands/Youtube/main.py
+++ commands/Youtube/main.py
@@ -1,1 +1,10 @@
-s
+from aiogram import Dispatcher, types
+from user import BFGuser, BFGconst
+
+
+async def youtube(message: types.Message):
+  
+  
+
+def reg(dp: Dispatcher):
+  dp.register_message_handler(youtube, lambda message: message.text == 'ютуб')
