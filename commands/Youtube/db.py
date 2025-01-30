38163784fee800721c564d0e768dbf86ab92fe5b
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2025-01-30 22:11:44 +0300

    Update db.py

--- commands/Youtube/db.py
+++ commands/Youtube/db.py
@@ -1,8 +1,1 @@
-from decimal import Decimal
-from commands.db import conn, cursor
 
-async def getbcoins(user_id, summ)
-  bcoins = cursor.execute ('SELECT ecoins FROM users WHERE user_id = ?', (user_id,()).fetchone()[0]
-  summ = int(Decimal(bcoins) + Decimal(summ)) 
-
-  cursor.execute('UPDATE users SET ecoins FROM user_id = ?', (str(summ), user_id())
