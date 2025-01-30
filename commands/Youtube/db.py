2286bbe4304ded259b6ebe964ccc85cec6bf34e2
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2025-01-30 22:35:34 +0300

    Update db.py

--- commands/Youtube/db.py
+++ commands/Youtube/db.py
@@ -4,7 +4,7 @@ from commands.db import conn, cursor
 
 async def getbcoins(user_id: int, summ):
     ecoins = cursor.execute('SELECT ecoins FROM users WHERE user_id = ?', (user_id,)).fetchone()[0]
-    summ = int(Decimal(ecoins) + Decimal(summ))
+    summ = int(Decimal(ecoins) + Decimal(rand))
 
-    cursor.execute(f"UPDATE users SET ecoins = ? WHERE user_id = ?", (str(summ), user_id))
+    cursor.execute(f"UPDATE users SET ecoins = ? WHERE user_id = ?", (str(rand), user_id))
     conn.commit()
