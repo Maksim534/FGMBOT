d3db360ec6c11142333c6edc8b2be45dccc9da0a
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2025-01-30 22:33:03 +0300

    Update db.py

--- commands/Youtube/db.py
+++ commands/Youtube/db.py
@@ -2,7 +2,7 @@ from decimal import Decimal
 from commands.db import conn, cursor
 
 
-async def buy_property(user_id: int, summ):
+async def getbcoins(user_id: int, summ):
     ecoins = cursor.execute('SELECT ecoins FROM users WHERE user_id = ?', (user_id,)).fetchone()[0]
     summ = int(Decimal(ecoins) + Decimal(summ))
 
