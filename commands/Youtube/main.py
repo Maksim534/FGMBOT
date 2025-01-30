f29590619d78d2c3ba8ee41dd73394f2ab0f465f
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2025-01-30 22:34:24 +0300

    Update main.py

--- commands/Youtube/main.py
+++ commands/Youtube/main.py
@@ -14,7 +14,7 @@ async def youtube(message: types.Message, user: BFGuser):
 
   rand = random.randint(1, 2)
 
-  await db.getbcoins(user_id: summ)
+  await db.getbcoins(user.user_id, summ)
   await message.answer(f'Вы успешно сняли видеоролик, вы получили {rand}, b-coins')
 
   
