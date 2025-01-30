6c1eaf3650419cf8206d84fbe03fe0fd304b7e65
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2025-01-30 21:55:23 +0300

    Update main.py

--- commands/Youtube/main.py
+++ commands/Youtube/main.py
@@ -1,8 +1,18 @@
 from aiogram import Dispatcher, types
 from user import BFGuser, BFGconst
+import random
+from assest.antispam import antispam
 
 
-async def youtube(message: types.Message):
+async def youtube(message: types.Message, user: BFGuser):
+  if int(user.property.phone) == 0:
+      await message.answer('у вас нет телефона чтобы снимать видеоролики')
+      return
+
+  rand = ['1', '2']
+  
+  await message.answer(f'Вы успешно сняли видеоролик, вы получили (random.choice{rand} B-coins')
+  db.getbcoins(user.user_id, summ)
   
   
 
