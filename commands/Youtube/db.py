b583c9cb3d9383254c03e579753aeccba4b6a84f
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2025-01-31 17:08:34 +0300

    Update db.py

--- commands/Youtube/db.py
+++ commands/Youtube/db.py
@@ -9,6 +9,6 @@ async def getbcoins(user_id: int, summ):
     cursor.execute(f"UPDATE users SET ecoins = ? WHERE user_id = ?", (str(summ), user_id))
     conn.commit()
 
-async def createkanal(user_id: int, num):
+async def createkanal(user_id):
     createkanal = cursor.execute(f"UPDATE users SET youtubekanal = ? WHERE user_id = ?", (0, user_id))
     conn.commit()
